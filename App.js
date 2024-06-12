f0a69051e5aeca2cb79242ae745f6e1aa3627187
Rully Damara <rullydamara@gmail.com>
2024-06-12 20:44:11 +0700

    develop feature to show weather data
    develop feature to show weather data from input search city function

--- App.js
+++ App.js
@@ -1,27 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { BASE_URL, API_KEY } from './src/constant'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, ActivityIndicator, Text } from 'react-native'
 import WeatherSearch from './src/components/weatherSearch'
 import WeatherInfo from './src/components/weatherInfo'
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState()
+  const [status, setStatus] = useState('')
   const searchWeather = (location) => {
+    setStatus('loading')
     axios
       .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
       .then((response) => {
         const data = response.data
-        console.log(data)
+        data.visibility /= 1000
+        data.visibility = data.visibility.toFixed(2)
+        data.main.temp -= 273.15 // Konversi Kelvin ke Celcius
+        data.main.temp = data.main.temp.toFixed(2)
+        setWeatherData(data)
+        setStatus('success')
       })
       .catch((error) => {
-        console.log(error)
+        setStatus('error')
       })
+  }
+
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size="large" />
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />
+      case 'error':
+        return (
+          <Text>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        )
+      default:
+        return
+    }
   }
 
   return (
     <View style={styles.container}>
       <WeatherSearch searchWeather={searchWeather} />
-      <WeatherInfo />
+      <View style={styles.margintTop20}>{renderComponent()}</View>
     </View>
   )
 }
--- src/components/weatherInfo.js
+++ src/components/weatherInfo.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
 
-const WeatherInfo = () => {
+const WeatherInfo = ({weatherData}) => {
   return (
     <View style={styles.marginTop20}>
-      <Text style={styles.text}>The weather of Jakarta</Text>
-      <Text style={[styles.temperature, styles.marginTop20]}>15 C</Text>
+      <Text style={styles.text}>The weather of {weatherData.name}</Text>
+      <Text style={[styles.temperature, styles.marginTop20]}>{weatherData.main.temp} C</Text>
       <View style={[styles.rowContainer, styles.marginTop20]}>
         <Image
-          source={{ uri: 'https://openweathermap.org/img/w/04d.png' }}
+          source={{ uri: `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png` }}
           style={styles.weatherIcon}
         />
-        <Text style={[styles.text, styles.bold]}>Clouds</Text>
+        <Text style={[styles.text, styles.bold]}>{weatherData.weather[0].main}</Text>
       </View>
-      <Text style={styles.text}>overcast clouds</Text>
+      <Text style={styles.text}>{weatherData.weather[0].description}</Text>
       <View style={[styles.rowContainer, styles.marginTop20]}>
         <Text style={[styles.text, styles.bold]}>Visibility :</Text>
-        <Text style={[styles.text, styles.marginLeft15]}>10 km</Text>
+        <Text style={[styles.text, styles.marginLeft15]}>{weatherData.visibility} km</Text>
       </View>
       <View style={[styles.rowContainer, styles.marginTop20]}>
         <Text style={[styles.text, styles.bold]}>Wind Speed :</Text>
-        <Text style={[styles.text, styles.marginLeft15]}>10 m/s</Text>
+        <Text style={[styles.text, styles.marginLeft15]}>{weatherData.wind.speed} m/s</Text>
       </View>
     </View>
   )
